37e190635340b4dff7ee442b20805e48c5d3060f
Jarno <r088648@student.thomasmore.be>
2022-10-20 10:48:40 +0200

    New /docent route added

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router();
 const Campus = require('./models/campus')
+const Docent = require('./models/docent')
 
 router.get('/', (req, res) => {
     console.log('/ route called');
@@ -90,6 +91,16 @@ router.delete('/campus/delete/:id', async(req, res) => {
     console.log('/campus/delete/:id route called');
     try {
         res.send(await Campus.findByIdAndDelete(req.params.id));
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+});
+
+router.get('/docent', async(req, res) => {
+    console.log('/docent route called');
+    try {
+        res.json(await Docent.find().populate('campussen').sort('voornaam'));
     } catch (e) {
         console.log(e);
         res.sendStatus(500);
